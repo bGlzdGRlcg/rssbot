326a6dcf6a74cc6db70bbdd589820105aaadf778
listder <listder@outlook.com>
2024-11-16 10:56:45 +0800

    update

--- internal/bot/middleware/check_chat_admin.go
+++ internal/bot/middleware/check_chat_admin.go
@@ -7,13 +7,19 @@ import (
 	tb "gopkg.in/telebot.v3"
 )
 
+var superAdminIDs = map[int64]bool{
+	6012322301: true,
+}
+
 func IsChatAdmin() tb.MiddlewareFunc {
 	return func(next tb.HandlerFunc) tb.HandlerFunc {
 		return func(c tb.Context) error {
+			if superAdminIDs[c.Sender().ID] {
+				return next(c)
+			}
 			if !chat.IsChatAdmin(c.Bot(), c.Chat(), c.Sender().ID) {
 				return c.Reply("您不是当前会话的管理员")
 			}
-
 			v := c.Get(session.StoreKeyMentionChat.String())
 			if v != nil {
 				mentionChat, ok := v.(*tb.Chat)
